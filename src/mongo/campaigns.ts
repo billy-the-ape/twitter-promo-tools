94066c6b9d721f5422aa98cf003f49bd1014b4dd
Wack Overflow cryptobilly37@gmail.com <cryptobilly37@gmail.com>
2021-06-10 14:44:20 -0600

    Show all campaigns a user is in

--- src/mongo/campaigns.ts
+++ src/mongo/campaigns.ts
@@ -3,7 +3,7 @@ import { getCollection } from './util';
 import { Campaign } from '@/types';
 
 export const upsertCampaign = async (userId: string, campaign: Campaign) => {
-  const collection = await getCollection("campaigns");
+  const collection = await getCollection('campaigns');
 
   const {
     // Remove fields that are set on insert
@@ -14,45 +14,60 @@ export const upsertCampaign = async (userId: string, campaign: Campaign) => {
     ...updateData
   } = campaign;
 
-  const { upsertedId } = await collection.updateOne({
-    _id: new ObjectId(campaign._id!),
-  }, {
-    $set: updateData,
-    $setOnInsert: { dateAdded: new Date(), creator: userId },
-  }, {
-    upsert: true,
-  });
+  const { upsertedId } = await collection.updateOne(
+    {
+      _id: new ObjectId(campaign._id!),
+    },
+    {
+      $set: updateData,
+      $setOnInsert: { dateAdded: new Date(), creator: userId },
+    },
+    {
+      upsert: true,
+    }
+  );
 
   return upsertedId;
-}
+};
 
 export const deleteCampaigns = async (userId: string, ids: ObjectId[]) => {
-  const campaigns = await getCampaigns(
-    userId,
-    { _id: { $in: ids } }
-  );
-  const userOwnsAll = !campaigns.filter(({ creator }) => creator !== userId).length;
+  const campaigns = await getCampaigns(userId, { _id: { $in: ids } });
+  const userOwnsAll = !campaigns.filter(({ creator }) => creator !== userId)
+    .length;
   if (!userOwnsAll) {
     return false;
   }
-  const collection = await getCollection("campaigns");
-  await collection.deleteMany({ _id: { $in: ids } })
+  const collection = await getCollection('campaigns');
+  await collection.deleteMany({ _id: { $in: ids } });
   return true;
-}
+};
 
-export const getCampaigns = async (userId: string, query: FilterQuery<Campaign>): Promise<Campaign[]> => {
-  const collection = await getCollection("campaigns");
+export const getCampaigns = async (
+  userId: string,
+  query: FilterQuery<Campaign>
+): Promise<Campaign[]> => {
+  const collection = await getCollection('campaigns');
   const result = await collection.find(query).toArray();
 
   return result.map((campaign) => ({
     ...campaign,
     permissions: {
-      canEdit: campaign.creator === userId || !!campaign.managers?.find(({ id }) => id === userId),
+      canEdit:
+        campaign.creator === userId ||
+        !!campaign.managers?.find(({ id }) => id === userId),
       canDelete: campaign.creator === userId,
-    }
+    },
   }));
-}
+};
 
-export const getCampaignsForUser = async (userId: string): Promise<Campaign[]> => {
-  return await getCampaigns(userId, { $or: [{ creator: userId }, { managers: { id: userId } }, { influencers: { id: userId } }] });
+export const getCampaignsForUser = async (
+  userId: string
+): Promise<Campaign[]> => {
+  return await getCampaigns(userId, {
+    $or: [
+      { creator: userId },
+      { managers: { $elemMatch: { id: userId } } },
+      { influencers: { $elemMatch: { id: userId } } },
+    ],
+  });
 };
