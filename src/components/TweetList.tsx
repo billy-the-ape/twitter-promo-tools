76cfe8d11902feb25ea3b52993a078a79ef9fd96
Wack Overflow cryptobilly37@gmail.com <cryptobilly37@gmail.com>
2021-06-14 22:43:21 -0600

    Add confirmation to delete tweet

--- src/components/TweetList.tsx
+++ src/components/TweetList.tsx
@@ -9,6 +9,11 @@ import {
   makeStyles,
 } from '@material-ui/core';
 import CancelIcon from '@material-ui/icons/Cancel';
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const ConfirmDialog = dynamic(() => import('./ConfirmDialog'), { ssr: false });
 
 export type TweetListProps = {
   tweets: SubmittedTweet[];
@@ -31,33 +36,46 @@ const useStyles = makeStyles(({ spacing, breakpoints }) => ({
 
 const TweetList: React.FC<TweetListProps> = ({ tweets, users, onDelete }) => {
   const classes = useStyles();
+  const [deleteId, setDeleteId] = useState('');
   const isMobile = useIsMobile();
+  const { t } = useTranslation();
+
   if (!tweets.length) {
     return <></>;
   }
   return (
-    <Box mt={2} display="flex" flexDirection="column">
-      {tweets.map(({ id, authorId }) => {
-        const { screenName, image } =
-          users.find(({ id }) => id === authorId) || {};
-        const link = `twitter.com/${screenName}/status/${id}`;
-        return (
-          <Box className={classes.linkBox} display="flex" alignItems="center">
-            <Avatar className={classes.linkAvatar} src={image!}>
-              {screenName?.substr(0, 1).toLocaleUpperCase()}
-            </Avatar>
-            <Link href={`https://${link}`} target="_blank">
-              <Typography variant="inherit">
-                {isMobile ? `...${link.substring(20)}` : link}
-              </Typography>
-            </Link>
-            <IconButton onClick={() => onDelete(id)} size="small">
-              <CancelIcon fontSize="small" />
-            </IconButton>
-          </Box>
-        );
-      })}
-    </Box>
+    <>
+      <Box mt={2} display="flex" flexDirection="column">
+        {tweets.map(({ id, authorId }) => {
+          const { screenName, image } =
+            users.find(({ id }) => id === authorId) || {};
+          const link = `twitter.com/${screenName}/status/${id}`;
+          return (
+            <Box className={classes.linkBox} display="flex" alignItems="center">
+              <Avatar className={classes.linkAvatar} src={image!}>
+                {screenName?.substr(0, 1).toLocaleUpperCase()}
+              </Avatar>
+              <Link href={`https://${link}`} target="_blank">
+                <Typography variant="inherit">
+                  {isMobile ? `...${link.substring(20)}` : link}
+                </Typography>
+              </Link>
+              <IconButton onClick={() => setDeleteId(id)} size="small">
+                <CancelIcon fontSize="small" />
+              </IconButton>
+            </Box>
+          );
+        })}
+      </Box>
+      {deleteId && (
+        <ConfirmDialog
+          open
+          title={t('delete_tweet')}
+          onConfirm={() => onDelete(deleteId)}
+          onClose={() => setDeleteId('')}
+        />
+      )}
+    </>
   );
 };
 
